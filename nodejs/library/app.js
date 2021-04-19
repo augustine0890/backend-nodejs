2041e2477e5799071d9b3c88522a5574af406edb
augustine <augustino0890@gmail.com>
2021-04-19 10:49:47 +0900

    nodejs: setup tools

--- nodejs/library/app.js
+++ nodejs/library/app.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const chalk = require('chalk');
+// eslint-disable-next-line no-unused-vars
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -17,6 +20,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname,  'views/index.html'));
 })
 
-app.listen(3000, function() {
-    console.log(`Listening on port ${chalk.green('3000')}`);
+app.listen(port, function() {
+    console.log(`Listening on PORT ${chalk.green(port)}`);
 })
