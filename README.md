6ae903a46016abc1e28f03374ded5fb6673b051a
augustine <augustino0890@gmail.com>
2021-04-19 16:23:54 +0900

    nodejs: event emitters

--- README.md
+++ README.md
@@ -45,6 +45,8 @@
 
 ## Server Sent Events (SSE)
 - Traditionally, a web page has to send a request to the server to receive new data; that is, the page requests data from the server. With server-sent events, it's possible for a server to send new data to a web page at any time, by pushing messages to the web page. These incoming messages can be treated as _Events_ + data inside the web page.
+### Event Emitters
+- Adding event listeners allow us create logic around future timelines, but the big difference is that events are expected to repeat.
 
 <!-- ## WebSockets -->
 <!-- - The server can send a message to the client without the client explicitly requesting something. -->
---
+++ async-await/fullstack/17-ee-create-emitter-custom-events.js
@@ -0,0 +1,35 @@
+const http = require('http');
+const readline = require('readline');
+const querystring = require('querystring');
+const EventEmitter = require('events');
+
+const rl = readline.createInterface({ input: process.stdin })
+
+rl.on('line', line => {
+  http.get(
+    `http://localhost:1337/chat?${querystring.stringify({ message: line })}`
+  )
+})
+
+const source = createEventSource('http://localhost:1337/sse');
+source.on('question', q => console.log(`Someone asked, "${q}"`))
+
+function createEventSource(url) {
+    const source = new EventEmitter()
+
+    http.get(url, res => {
+        res.on('data', data => {
+            const message = data
+                .toString()
+                .replace(/^data: /, '')
+                .replace(/\n\n$/, '');
+            
+            source.emit('message', message)
+
+            const eventType = message.match(/\?$/) ? 'question' : 'statement';
+            source.emit(eventType, message)
+        })
+    })
+
+    return source
+}
