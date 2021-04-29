ba892259a4c7764c0d4dcedbac888c88026cff43
augustine <augustino0890@gmail.com>
2021-04-29 15:41:20 +0900

    template engines: pug and js

--- nodejs/library/app.js
+++ nodejs/library/app.js
@@ -15,9 +15,12 @@ app.use(express.static(path.join(__dirname, '/public/')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname,  'views/index.html'));
+    // res.sendFile(path.join(__dirname,  'views/index.html'));
+    res.render('index', { list: ['a', 'b'] });
 })
 
 app.listen(port, function() {
