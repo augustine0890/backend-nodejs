ea762fe89202db4b18ddaad0906eb2fea65b5f2e
augustine <augustino0890@gmail.com>
2021-04-29 16:20:35 +0900

    ejs template engine

--- nodejs/library/app.js
+++ nodejs/library/app.js
@@ -16,11 +16,12 @@ app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dis
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
-app.set('view engine', 'pug');
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
     // res.sendFile(path.join(__dirname,  'views/index.html'));
-    res.render('index', { list: ['a', 'b'] });
+    res.render('index', { list: ['a', 'b'], title: 'Library' });
 })
 
 app.listen(port, function() {
