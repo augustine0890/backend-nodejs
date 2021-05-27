52add577094dc85ac927f40f8eaa40cb8aa75fa7
augustine <augustino0890@gmail.com>
2021-05-27 18:21:48 +0900

    marketplace: user model and auth

--- projects/marketplace/server/server.js
+++ projects/marketplace/server/server.js
@@ -1,10 +1,21 @@
 import config from './../config/config';
 import app from './express';
+const chalk = require('chalk');
 import mongoose from 'mongoose';
 
 // Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri)
+mongoose.set('useCreateIndex', true);
+mongoose
+  .connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  })
+  .then(() =>
+    console.log(`${chalk.green('✓')} ${chalk.blue('MongoDB Connected!')}`)
+  );
+
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 })
@@ -13,5 +24,9 @@ app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
   }
-  console.info('Server started on port %s.', config.port)
+  console.log(
+    `${chalk.green('✓')} ${chalk.blue(
+      `Listening on port ${config.port}. Visit http://localhost:${config.port}/ in your browser.`
+    )}`
+  );
 })
