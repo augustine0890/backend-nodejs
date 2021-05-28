84e5bc1f3b7405ce63bcbae41fd016e5677c94e7
augustine <augustino0890@gmail.com>
2021-05-28 16:37:46 +0900

    ts: types

--- typescript/typechain/index.ts
+++ typescript/typechain/index.ts
@@ -1,1 +1,13 @@
 console.log("Hello World!");
+
+const yourName = "Augustine",
+    age = 31,
+    gender = "male";
+
+const sayHi = (yourName, age, gender?) => {
+    console.log(`Hello ${yourName}, you are ${age}, and you are a ${gender}.`);
+}
+
+sayHi(yourName, age, gender);
+
+export {};
