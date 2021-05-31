313eeb01976b4341586034790fc6acb718394d81
augustine <augustino0890@gmail.com>
2021-05-31 18:37:10 +0900

    nestjs: create REST API

--- nestjs/iluvcoffee/src/app.module.ts
+++ nestjs/iluvcoffee/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CoffeeController } from './coffee/coffee.controller';
+import { CoffeesController } from './coffees/coffees.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CoffeeController],
+  controllers: [AppController, CoffeesController],
   providers: [AppService],
 })
 export class AppModule {}
