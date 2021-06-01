d6c77e65f2f8d8ded74869a2da057c35284ee5f9
augustine <augustino0890@gmail.com>
2021-06-01 12:02:49 +0900

    Create a REST API app

---
+++ nestjs/iluvcoffee/src/coffees/coffees.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((item) => item.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee #${id} not found`);
+    }
+    return coffee;
+  }
+
+  create(createCoffee: any) {
+    this.coffees.push(createCoffee);
+  }
+
+  update(id: string, updateCoffee: any) {
+    const coffee = this.findOne(id);
+    if (coffee) {
+      console.log(updateCoffee);
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
--- nestjs/note.md
+++ nestjs/note.md
@@ -10,3 +10,9 @@
 - Generates and/or modifies files based on a schematic
     - `nest generate <schematic> <name> [options]`
         - `nest g co`
+- Create validation decorations
+    - `yarn add class-validator`
+- Transform plain object to some instance of class and versa
+    - `yarn add class-transformer`
+- Mapped Types
+    - `yarn add @nestjs/mapped-types`
