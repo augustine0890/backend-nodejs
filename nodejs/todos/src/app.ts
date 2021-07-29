a8c9728deb56a6eb311e2237894646bab2711501
augustine <augustino0890@gmail.com>
2021-07-29 12:17:23 +0900

    swagger ui express

--- nodejs/todos/src/app.ts
+++ nodejs/todos/src/app.ts
@@ -1,11 +1,20 @@
+import path from 'path'
 import express, { Request, Response, NextFunction } from 'express';
 import todoRoutes from './routes/todos';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs'
 
 const app = express();
+const fileName = path.join(__dirname, '..', 'src/playdapp.yaml')
+
+const swaggerDocument = YAML.load(fileName)
 
 app.use(express.json());
 
 app.use('/todos', todoRoutes);
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
