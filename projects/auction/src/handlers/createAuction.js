3e85bc4ab0bc906ee89faca0466bec099ffad935
augustine <augustino0890@gmail.com>
2021-10-07 15:04:41 +0900

    auction service: dynamoDB table, IAM Role, and optimize serverless yaml

--- projects/auction/src/handlers/createAuction.js
+++ projects/auction/src/handlers/createAuction.js
@@ -1,13 +1,23 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title: title,
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
 
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
 
   return {
     statusCode: 201,
